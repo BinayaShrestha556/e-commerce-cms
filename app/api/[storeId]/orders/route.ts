d5a98f31a6821b826b4078d34d8ae9466932b290
BinayaShrestha556 <binayastha37b@gmail.com>
2025-01-27 21:02:45 +0545

    some updates

--- app/api/[storeId]/orders/route.ts
+++ app/api/[storeId]/orders/route.ts
@@ -11,21 +11,31 @@ export async function POST(
        const userId=user?.id
     if (!userId) return new NextResponse("unauthenticated", { status: 401 });
     const body = await req.json();
-    const { name,value } = body;
-    if (!name) return new NextResponse("name is required", { status: 401 });
-    if (!value) return new NextResponse("value url is required", { status: 401 });
-    if (!params.storeId)
-      return new NextResponse("store id is required", { status: 400 });
-    const storeByUserId=await prismadb.store.findFirst({
-        where:{
-            id:params.storeId,
-            userId
-        }
-    })
-    if(!storeByUserId){
-        return new NextResponse("unauthorized",{status:403})
-    }
+    const {orderItems,address,phone}=body
+    const {storeId}=params
+   const order= await prismadb.order.create({
+      data:{
+        userId,
+        storeId,
+        address,
+        phone,
+        isPaid:true
+      }
 
+
+    })
+    const items=orderItems.map((e:any)=>({
+        orderId:order.id,
+        productId:e.productId,
+        sizeId:e.sizeId,
+        numberOfItem:e.numberOfItem
+
+
+    }))
+    await prismadb.orderItems.createMany({
+        data:items
+    })
+    
  
   } catch (error) {
     console.log("[orders_POST]", error);
