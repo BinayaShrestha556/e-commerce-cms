a19977ab870f8491752bc95282b4f03e1d337685
BinayaShrestha556 <binayastha37b@gmail.com>
2025-02-03 16:25:48 +0545

    attempt to resolve cors error in signout

--- app/api/users-logout/route.ts
+++ app/api/users-logout/route.ts
@@ -1,10 +1,20 @@
 import { signOut } from "@/auth";
 
-import {  NextResponse } from "next/server";
+import {  NextRequest, NextResponse } from "next/server";
 
-export async function POST(){
-    
-        await signOut({redirect:false})
+export async function POST(req:NextRequest){
+    try {
+            const headers = {
+                    "Access-Control-Allow-Origin": req.headers.get("origin") || "*", // Allow the requesting origin
+                    "Access-Control-Allow-Methods": "POST, OPTIONS", // Allow POST and OPTIONS methods
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow necessary headers
+                    "Access-Control-Allow-Credentials": "true", // Allow credentials
+                  };
+            await signOut({redirect:false})
+            
+            return NextResponse.json({message:"logged out"},{status:200,headers})
+    } catch (error) {
+        return NextResponse.json("Something ernt wrong",{status:500})
         
-        return NextResponse.json({message:"logged out"},{status:200})
+    }
 }
