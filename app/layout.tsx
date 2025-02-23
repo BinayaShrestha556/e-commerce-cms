d46dd2194d742bcaff8f2973644ab5300028fbf1
BinayaShrestha556 <binayastha37b@gmail.com>
2025-02-23 12:25:37 +0545

    changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Store CMS",
+  title: "OmniCMS",
   description: "Create your own store, backend for all your store.",
 };
 
