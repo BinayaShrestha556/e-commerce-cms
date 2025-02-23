85725539ffadeaea9e9ede08c89c4025e48d0bed
BinayaShrestha556 <binayastha37b@gmail.com>
2025-02-23 11:58:39 +0545

    improved navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,37 @@
+import { MainNav } from "./mainNav";
+import StoreSwitcher from "./storeSwitcher";
 
-import { MainNav } from "./mainNav"
-import StoreSwitcher from "./storeSwitcher"
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-import { redirect } from "next/navigation"
-import prismadb from "@/lib/prismadb"
+import { UserOptions } from "./user-options";
+import { SessionProvider } from "next-auth/react";
+import { useServerUser } from "@/hooks/use-server-user";
 
-import { UserOptions } from "./user-options"
-import { SessionProvider } from "next-auth/react"
-import { useServerUser } from "@/hooks/use-server-user"
+const Navbar = async () => {
+  const user = await useServerUser();
+  const userId = user?.id;
 
-
-const Navbar =async () => {
-   const user =await useServerUser();
-    const userId=user?.id
-
-  
-  if(!userId) redirect("/sign-in")
-  const stores=await prismadb.store.findMany({
-where:{
-  userId
-}})
+  if (!userId) redirect("/sign-in");
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
-    <div className="border-b h-12 flex items-center px-5 w-full">
-      <StoreSwitcher items={stores}/>
+    <div className="border-b h-12 flex items-center px-5 w-full ">
+      <p className="font-serif text-2xl font-bold tracking-tighter ">OmniCMS</p>
       <div className=" flex-1 flex justify-end md:justify-start pr-2">
-      <MainNav/>
+        <MainNav />
       </div>
-      <div >
-      <SessionProvider><UserOptions/></SessionProvider></div>
+      <div className="flex items-center gap-4">
+        <StoreSwitcher items={stores} />
+        <SessionProvider>
+          <UserOptions />
+        </SessionProvider>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
