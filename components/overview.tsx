09541bf2051b6e9ec5c781344f286ea85c503e4f
BinayaShrestha556 <binayastha37b@gmail.com>
2025-06-25 14:09:46 +0545

    overview dark mode bug

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,23 +1,40 @@
-"use client"
-import { GraphData } from "@/actions/get-graph-revenue"
-import { useTheme } from "next-themes"
-
-interface OverviewProps{
-    data : GraphData[]
+"use client";
+import { GraphData } from "@/actions/get-graph-revenue";
+import cookie from "js-cookie";
+interface OverviewProps {
+  data: GraphData[];
 }
-import {Bar, BarChart, ResponsiveContainer, XAxis, YAxis} from "recharts"
-const Overview:React.FC<OverviewProps>=({data})=>{
-    const theme=useTheme()
-    return(
-        <ResponsiveContainer width="100%" height={350}>
-            <BarChart data={data}>
-                <XAxis dataKey="name" stroke="#888888" fontSize={12} tickLine={false} axisLine={false}/>
-                <YAxis tickFormatter={(value)=>`$${value}`} dataKey="total" stroke="#888888" fontSize={12} tickLine={false} axisLine={false}/>
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+const Overview: React.FC<OverviewProps> = ({ data }) => {
+  const theme = cookie.get("mode") || "light";
 
-        <Bar dataKey="total" fill={theme.resolvedTheme==="dark"?"#faf4eb":"#000000"} className="bg-background" radius={[4,4,0,0]}/>
-            </BarChart>
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          tickFormatter={(value) => `$${value}`}
+          dataKey="total"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
 
-        </ResponsiveContainer>
-    )
-}
-export default Overview
+        <Bar
+          dataKey="total"
+          fill={theme === "dark" ? "#faf4eb" : "#000000"}
+          className="bg-background"
+          radius={[4, 4, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+export default Overview;
